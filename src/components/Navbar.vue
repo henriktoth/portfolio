0a9e694c6cb5d01a0094e0248e83db18f6681ec1
Tóth Henrik <henriktoth0517@HenrikMac.local>
2025-03-18 18:12:29 +0100

    feat(navbar): making navbar responsive

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,11 +1,25 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, onMounted, onBeforeUnmount} from 'vue';
 const isNavOpen = ref(false);
+const windowWidth = ref(window.innerWidth);
 defineProps({
     links: {
         type: Array
     }
 })
+
+function updateWindowWidth(){
+    windowWidth.value = window.innerWidth;
+}
+
+onMounted(() => {
+    window.addEventListener('resize', updateWindowWidth);
+})
+
+onBeforeUnmount(() => {
+    window.removeEventListener('resize', updateWindowWidth);
+})
+
 function toggleNav() {
     isNavOpen.value = !isNavOpen.value;
     
@@ -18,18 +32,29 @@ function toggleNav() {
 
 <template>
     <nav class="">
+        
         <div id="navbar" class="bg-neutral-800 m-2 rounded-2xl p-5 flex justify-between">
+            
             <div class="flex flex-col max-w-40 items-center">
                 <h1 class="text-2xl font-bold tracking-wide">PORTFOLIO</h1>
                 <p class="text-[12px] bg-neutral-600 p-1 rounded-lg tracking-[0.4em]">TÓTH HENRIK</p>
             </div>
-            <button @click="toggleNav" class=" cursor-pointer text-5xl relative bottom-1.5">☰</button>
+
+            <button v-if="windowWidth < 900" @click="toggleNav" class="cursor-pointer text-5xl relative bottom-1.5">☰</button>
+            <ul class="flex gap-3" v-else >
+                <li v-for="link in links" class="hover:bg-neutral-600 px-2 py-3 rounded-xl w-[100%] flex items-center text-[14px]">
+                <a :href="link.url">{{ link.text }}</a>
+            </li>
+            </ul>
+            
         </div>
+        
         <ul v-if="isNavOpen" id="navbar-list"
             class="bg-neutral-800 m-2 rounded-2xl p-5 flex flex-col items-center text-sm">
             <li v-for="link in links" class="hover:bg-neutral-600 px-2 py-3 rounded-xl w-[100%] text-center">
                 <a :href="link.url">{{ link.text }}</a>
             </li>
         </ul>
+    
     </nav>
 </template>
