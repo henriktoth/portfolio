4ffb848177dbdb780b15cb20f9aec7815e86d446
Tóth Henrik <henriktoth0517@HenrikMac.local>
2025-03-18 16:43:43 +0100

    feat(navbar): added hamburger menu to mobile view

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -8,6 +8,11 @@ defineProps({
 })
 function toggleNav() {
     isNavOpen.value = !isNavOpen.value;
+    
+    const navButton = document.querySelector('#navbar button');
+    
+    navButton.textContent = isNavOpen.value ? 'x' : '☰';
+    navButton.classList.toggle('right-2.5', isNavOpen.value);
 }
 </script>
 
@@ -17,7 +22,8 @@ function toggleNav() {
             <div class="flex flex-col max-w-40 items-center">
                 <h1 class="text-2xl font-bold tracking-wide">PORTFOLIO</h1>
                 <p class="text-[12px] bg-neutral-600 p-1 rounded-lg tracking-[0.4em]">TÓTH HENRIK</p>
-            </div><button @click="toggleNav" class="border cursor-pointer">nav_toggle</button>
+            </div>
+            <button @click="toggleNav" class=" cursor-pointer text-5xl relative bottom-1.5">☰</button>
         </div>
         <ul v-if="isNavOpen" id="navbar-list"
             class="bg-neutral-800 m-2 rounded-2xl p-5 flex flex-col items-center text-sm">
