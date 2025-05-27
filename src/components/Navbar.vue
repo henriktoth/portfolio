055a9e64d3515270fb7fc1adc6614344a155e40f
henriktoth <henriktoth0517@gmail.com>
2025-05-28 00:28:14 +0200

    feat: added darkModeButton to navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { ref, onMounted, onBeforeUnmount } from 'vue';
-
+import DarkModeSwitch from './DarkModeSwitch.vue';
 
 const isNavOpen = ref(false);
 const windowWidth = ref(window.innerWidth);
@@ -25,7 +25,7 @@ onBeforeUnmount(() => {
 
 function toggleNav() {
     isNavOpen.value = !isNavOpen.value;
-    const navButton = document.querySelector('#navbar button');
+    const navButton = document.querySelector('#nav-button');
     navButton.textContent = isNavOpen.value ? 'x' : '☰';
     navButton.classList.toggle('right-2.5', isNavOpen.value);
 }
@@ -33,14 +33,17 @@ function toggleNav() {
 
 <template>
     <nav class="sticky top-2 z-100">
-
         <div id="navbar" class="bg-neutral-800 m-2 rounded-2xl p-5 flex justify-between">
-            <div class="flex flex-col max-w-40 items-center">
-                <h1 class="text-2xl font-bold tracking-wide">PORTFOLIO</h1>
-                <p class="text-[12px] bg-neutral-600 p-1 rounded-lg tracking-[0.4em]">TÓTH HENRIK</p>
+            <div class="flex flex-row gap-5">
+                <div class="flex flex-col max-w-40 items-center">
+                    <h1 class="text-2xl font-bold tracking-wide">PORTFOLIO</h1>
+                    <p class="text-[12px] bg-neutral-600 p-1 rounded-lg tracking-[0.4em]">TÓTH HENRIK</p>
+                </div>
+                <div class="flex flex-col justify-center">
+                    <DarkModeSwitch class="max-h-[70%]"/>
+                </div>
             </div>
-
-            <button v-if="windowWidth < 900" @click="toggleNav"
+            <button id="nav-button" v-if="windowWidth < 900" @click="toggleNav"
                 class="cursor-pointer text-5xl relative bottom-1.5">☰</button>
             <ul  v-else class="flex gap-3">
                 <li v-for="link in links"
