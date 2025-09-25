8e92bea551b7ce08bf5328a78bc2f65f49af533b
henriktoth <henriktoth0517@gmail.com>
2025-09-25 17:50:30 +0200

    feat: added demo for burgerco

--- src/components/DarkModeSwitch.vue
+++ src/components/DarkModeSwitch.vue
@@ -11,15 +11,9 @@ const toggleDarkMode = () => {
 </script>
 
 <template>
-    <button 
-        @click="toggleDarkMode" 
-        class=" p-2 rounded-full bg-neutral-800 dark:bg-neutral-200 transition-colors duration-200 hover:cursor-pointer"
-    >
-        <Icon 
-            :icon="isDarkMode ? 'ph:sun-bold' : 'ph:moon-bold'" 
-            class="text-white dark:text-neutral-800" 
-            width="24" 
-            height="24"
-        />
+    <button @click="toggleDarkMode"
+        class=" p-2 rounded-full bg-neutral-800 dark:bg-neutral-200 transition-colors duration-200 hover:cursor-pointer">
+        <Icon :icon="isDarkMode ? 'ph:sun-bold' : 'ph:moon-bold'" class="text-white dark:text-neutral-800" width="24"
+            height="24" />
     </button>
-</template> 
+</template>
